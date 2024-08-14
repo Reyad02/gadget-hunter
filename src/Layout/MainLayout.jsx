b33d9abf66c215ff80406baa90fc98b9cc6f3773
unknown <areyad203011@bscse.uiu.ac.bd>
2024-08-14 21:12:19 +0600

    layout added, project necessary components added

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,8 +1,21 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
 
 const MainLayout = () => {
     return (
         <div>
-            this is main layout
+            <div className="max-w-7xl mx-auto">
+                <div>
+                    <Navbar></Navbar>
+                </div>
+                <div className="">
+                    <Outlet></Outlet>
+                </div>
+            </div>
+            <div>
+                <Footer></Footer>
+            </div>
         </div>
     );
 };
--- src/Routes/route.jsx
+++ src/Routes/route.jsx
@@ -1,11 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
+import Home from "../Pages/Home/Home";
+import About from "../Pages/About/About";
+import Contact from "../Pages/Contact/Contact";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout></MainLayout>,
-    //   children: 
+      children: [
+        {
+            path: "/",
+            element: <Home></Home>
+        },
+        {
+            path: "/about",
+            element: <About></About>
+        },
+        {
+            path: "/contact",
+            element: <Contact></Contact>
+        },
+      ] 
     },
   ]);
 
